c0ba7307231d3de1f9880528c5f7c28a549c8bce
haihaihai103 <80326285+haihaihai103@users.noreply.github.com>
2021-07-27 09:32:28 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'patients#index'
   get '/users', to: 'devise/registrations#new'
+  resources :patients do
+    get :chart, on: :collection
+    #get :collection, on: :collection
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_111921) do
+ActiveRecord::Schema.define(version: 2021_07_15_062101) do
+
+  create_table "patients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "kana01"
+    t.string "name01", null: false
+    t.string "gender", null: false
+    t.integer "age"
+    t.string "nationality"
+    t.integer "postcode"
+    t.string "prefecture_id"
+    t.string "city"
+    t.string "town"
+    t.integer "phone01"
+    t.integer "phone02"
+    t.string "email"
+    t.string "belong_id", null: false
+    t.string "belong01_id"
+    t.string "belong02_id"
+    t.string "belong03_id"
+    t.string "company"
+    t.string "location"
+    t.integer "phone03"
+    t.string "kana02"
+    t.string "name02"
+    t.string "relationship"
+    t.integer "phone04"
+    t.integer "phone05"
+    t.integer "number"
+    t.text "free"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +63,5 @@ ActiveRecord::Schema.define(version: 2021_07_12_111921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "patients", "users"
 end
